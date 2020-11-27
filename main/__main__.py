e89646a08c314e74f0a57766aa9a47c5990432a4
Dmitry Grishin <grishinda10@ya.ru>
2020-11-27 21:04:17 +0500

    дописать тесты

--- main/__main__.py
+++ main/__main__.py
@@ -1,12 +1,58 @@
-# KEYS.BIN
+import argparse
 
-# КЛЮЧ -
-# 64 БАЙТА +
-# 8 БАЙТ ОТСТУПОВ
-#
-# VALUES.BIN
+from main.storage import Storage
 
-# ЗНАЧЕНИЕ -
-# 4 БАЙТА ДЛИНЫ ЗНАЧЕНИЯ
-# + N БАЙТ ЗНАЧЕНИЯ
-print('Ну привет')
+
+def long_session():
+    print('Начало сессии')
+    print('===================================')
+    while True:
+        x = input()
+        if x == 'set':
+            storage.set_data(input('Введите ключ: '),
+                             input('Введите значение: '))
+        elif x == 'get':
+            storage.get(input('Ключ: '))
+        elif x == 'del':
+            storage.delete(input('Ключ: '))
+        elif x == 'exit':
+            print('===================================')
+            break
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='KV-storage')
+    parser.set_defaults(command='zero_args')
+
+    subparsers = parser.add_subparsers()
+
+    set_parser = subparsers.add_parser('set', help='Добавление данных')
+    set_parser.add_argument('key', help='Ключ', type=str)
+    set_parser.add_argument('value', help='Значение', type=str)
+    set_parser.set_defaults(command="set")
+
+    get_parser = subparsers.add_parser('get', help='Получение значения')
+    get_parser.add_argument('key', help='Ключ', type=str)
+    get_parser.set_defaults(command="get")
+
+    long_parser = subparsers.add_parser('long', help='сессия в консоли')
+    long_parser.set_defaults(command="long")
+
+    del_parser = subparsers.add_parser('del', help='Удаление значения')
+    del_parser.add_argument('key', help='Ключ', type=str)
+    del_parser.set_defaults(command="del")
+    args = parser.parse_args()
+
+    storage = Storage('keys.bin', 'values.bin')
+    command = args.command
+    try:
+        if command == 'set':
+            storage.set_data(args.key, args.value)
+        elif command == 'get':
+            storage.get(args.key)
+        elif command == 'del':
+            storage.delete(args.key)
+        elif command == 'long':
+            long_session()
+    except KeyError:
+        exit("Был введен неверный ключ")
--- main/storage.py
+++ main/storage.py
@@ -1,11 +1,21 @@
 import hashlib
-from typing import NoReturn
+from pathlib import Path
+from typing import NoReturn, Union
 
 
 class Storage:
     def __init__(self, key_file: str, value_file: str):
         self.key_file = key_file
         self.value_file = value_file
+        self._check_file_exists()
+
+    def _check_file_exists(self) -> NoReturn:
+        key = Path(self.key_file)
+        value = Path(self.value_file)
+        if not key.exists():
+            key.touch()
+        if not value.exists():
+            value.touch()
 
     @staticmethod
     def get_hash(x: str) -> bytes:
@@ -20,18 +30,19 @@ class Storage:
     def from_bytes(value: bytes) -> int:
         return int.from_bytes(value, byteorder='big')
 
-    def _find_key(self, finder: str) -> bool:
+    def _find_key(self, finder: str) -> Union[bool, int]:
         with open(self.key_file, 'rb') as f:
             while True:
                 packet = f.read(72)
                 key, offset = packet[:64], self.from_bytes(packet[64:])
                 if key == self.get_hash(finder):
-                    return True
+                    return offset
                 if key == b'' and offset == 0:
                     return False
 
-    def put(self, key: str, value: str) -> NoReturn:
-        if not self._find_key(key):
+    def set_data(self, key: str, value: str) -> NoReturn:
+        result = self._find_key(key)
+        if result is False:
             with open(self.value_file, 'ab') as f:
                 pos = f.tell()
                 f.write(self.to_bytes(len(value), 4))
@@ -40,9 +51,25 @@ class Storage:
                 f.write(self.get_hash(key))
                 f.write(self.to_bytes(pos, 8))
         else:
-            print('ключ уже существует')
+            self.delete(key)
+            with open(self.value_file, 'rb+') as f:
+                f.seek(result)
+                pos = f.tell()
+                length = f.read(4)
+                if len(value) < self.from_bytes(length):
+                    f.seek(-4, 1)
+                    f.write(self.to_bytes(len(value), 4))
+                    f.write(value.encode())
+                else:
+                    with open(self.value_file, 'ab') as a:
+                        pos = a.tell()
+                        a.write(self.to_bytes(len(value), 4))
+                        a.write(value.encode())
+            with open(self.key_file, 'ab') as f:
+                f.write(self.get_hash(key))
+                f.write(self.to_bytes(pos, 8))
 
-    def get(self, find_key: str) -> str:
+    def get(self, find_key: str) -> NoReturn:
         with open(self.key_file, 'rb') as f:
             while True:
                 packet = f.read(72)
@@ -52,30 +79,32 @@ class Storage:
                         a.seek(offset)
                         length = self.from_bytes(a.read(4))
                         value = a.read(length)
-                        return value.decode()
+                        print(f'значение: {value.decode()}')
+                        break
                 if key == b'' and offset == 0:
                     print('Ключ не найден')
                     break
 
     def delete(self, find_key: str) -> NoReturn:
+        new_data = None
         with open(self.key_file, 'rb+') as f:
             data = f.read()
             f.seek(0)
             while True:
-                pos = f.tell()
+                start = f.tell()
                 packet = f.read(72)
+                end = f.tell()
                 key, offset = packet[:64], self.from_bytes(packet[64:])
                 if key == self.get_hash(find_key):
-                    new_data = data[:pos] + data[pos + 72:]
-                    f.write(new_data)
-                    print("Успешно")
+                    new_data = data[:start] + data[end:]
                     break
                 if key == b'' and offset == 0:
                     print('ключ не найден')
                     break
-
-
-stor = Storage('keys.bin', 'values.bin')
-stor.put('sas', '123')
-print(stor.get('sas'))
-stor.delete('sas')
+        if new_data is not None:
+            p = Path(self.key_file)
+            p.unlink(True)
+            temp = Path('temp.bin')
+            temp.touch()
+            temp.write_bytes(new_data)
+            temp.rename(self.key_file)
